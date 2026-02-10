463fbbd33128c5f8d4599c185ebbd4400137fc13
Krishna <krishkal9294@gmail.com>
2026-02-10 22:12:02 +0530

    understood cors,api.use,middleware

--- src/index.js
+++ src/index.js
@@ -8,8 +8,14 @@ dotenv.config(
 )
 
 
-connectDB();
-
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT, () => { console.log(`Server is running on port ${process.env.PORT}`)
+})
+})
+.catch((error) => {console.log("Error connecting to DB", error);
+    process.exit(1);
+})
 
 
 
